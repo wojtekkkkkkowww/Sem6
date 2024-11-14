50580536260d5cace4dfe69e37bbc7c8cf345fd1
wojtekkkkkkowww <116020017+wojtekkkkkkowww@users.noreply.github.com>
2024-11-14 18:45:46 +0100

    Update zad1.py

--- wza/zad1.py
+++ wza/zad1.py
@@ -1,45 +1,22 @@
-class GaussInt:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+declare -A stats
 
-    def norm(self):
-        return self.a*self.a + self.b*self.b
-    
-    def __str__(self):
-        return f"{self.a} + {self.b}i"
+#do zmiennej zapisuję listę plików, które potem będę chciał wyświetlać i przeszukiwać
+FILES="$(svn ls -r $1 $2 --depth=infinity | grep '[^\/]$' | awk -v a="$1" -v b="$2" '{print b$0"@"a}' )"
 
-    def __add__(self, other):
-        return GaussInt(self.a + other.a, self.b + other.b)
+#iteruję w pętli po plikach i wewnątrz pilku po słowach (dzięki temu jeden plik jest zaciągany tylko raz!)
+for FILE in `echo "$FILES"`
+do
+	for WORD in `echo "$FILE" | xargs svn cat`
+	do
+		if [ stats[$WORD] ]; then
+			((stats[$WORD]++))
+		else
+			stats[$WORD]=1
+		fi
+	done
+done
 
-    def __sub__(self, other):
-        return GaussInt(self.a - other.a, self.b - other.b)
-    
-    def __mul__(self, other):
-        return GaussInt(self.a * other.a - self.b * other.b, self.a * other.b + self.b * other.a)
-    
-    def __truediv__(self, other):
-        if other.a == 0 and other.b == 0:
-            return []
-        norm = other.norm()
-        a = (self.a*other.a + self.b*other.b)/norm
-        b = (self.b*other.a - self.a*other.b)/norm
-        q = GaussInt(round(a),round(b))
-        return (q, self - (q * other))
-
-def gcd(x, y):
-    if y.a == 0 and y.b == 0:
-        return x
-    q,r = x/y
-    return gcd(y, r)    
-
-def lcm(x,y):
-    q,r = (x*y)/gcd(x,y)
-    return q
-    
-# test
-x = GaussInt(3,4)
-y = GaussInt(1,3)
-
-print(f"c = {gcd(x,y)}")
-print(f"d = {lcm(x,y)}")
+for WORD in `echo "${!stats[@]}" | tr " " "\n" | sort`; #wyświetlamy tablicę z liczbą wystąpień w sposób posorotowany leksykograficznie
+do
+	echo "$WORD | ${stats[$WORD]}"
+done
